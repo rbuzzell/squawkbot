4580357051d4c2ee717b753afc7ee34f4615a458
Ryan Buzzell <ryan@buzzell.io>
2021-07-08 21:25:40 -0400

    Added example code for env variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+    print_env();
+}
+
+fn print_env(){
+    dotenv().ok();
+
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
 }
