b8c195c8ff7069e4a0021ab94e6600019f0a8f27
Ryan Buzzell <ryan@buzzell.io>
2021-07-29 21:31:02 -0400

    blacked file

--- src/storage.py
+++ src/storage.py
@@ -8,23 +8,25 @@ from sqlalchemy import Table, Column, Integer, String, MetaData
 
 class Storage:
     def __init__(self):
-        os.makedirs('sqlite', exist_ok=True)  # creates directory for db
-        engine = db.create_engine('sqlite:///sqlite/storage.db')
+        os.makedirs("sqlite", exist_ok=True)  # creates directory for db
+        engine = db.create_engine("sqlite:///sqlite/storage.db")
         connection = engine.connect()
         meta = MetaData()
         metrics = Table(
-            'metrics', meta,
-            Column('id', Integer, primary_key=True),
-            Column('server_name', String),
-            Column('user', String),
-            Column('high_score', Integer)
+            "metrics",
+            meta,
+            Column("id", Integer, primary_key=True),
+            Column("server_name", String),
+            Column("user", String),
+            Column("high_score", Integer),
         )
         current_count = Table(
-            'current_count', meta,
-            Column('id', Integer, primary_key=True),
-            Column('server_name', String),
-            Column('user', String),
-            Column('current_score', Integer)
+            "current_count",
+            meta,
+            Column("id", Integer, primary_key=True),
+            Column("server_name", String),
+            Column("user", String),
+            Column("current_score", Integer),
         )
         meta.create_all(engine)
 
