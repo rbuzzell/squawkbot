dba2b646719cc8b718d142bf31253a735aef79bd
Rob Glossop <robgssp@gmail.com>
2022-06-22 23:35:24 +0000

    switch to a synchronous sqlite library

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import * as D from "discord.js";
 import axios from "axios";
-import * as sqlite3 from "sqlite3";
-import * as sqlite from "sqlite";
+import * as sqlite from "better-sqlite3";
+import Database from "better-sqlite3";
 import { Mutex } from "async-mutex";
 
 var debug = require('debug')('squawk');
 
-async function setupDb(ctx: Context) {
-  await ctx.db.exec(`CREATE TABLE IF NOT EXISTS count (
+function setupDb(ctx: Context): void {
+  ctx.db.exec(`CREATE TABLE IF NOT EXISTS count (
 guild TEXT NOT NULL PRIMARY KEY,
 count INTEGER NOT NULL,
 lastbumped TEXT)
 STRICT`);
 
-  await ctx.db.exec(`CREATE TABLE IF NOT EXISTS stats (
+  ctx.db.exec(`CREATE TABLE IF NOT EXISTS stats (
 guild TEXT NOT NULL,
 user TEXT NOT NULL,
 bumps INTEGER NOT NULL,
@@ -26,14 +26,12 @@ type CountResult = 'bump' | 'ignore' | 'loss';
 
 type Context = {
   db: sqlite.Database,
-  dbLock: Mutex,
   discord: D.Client,
 };
 
-async function currentCount(ctx: Context, guild: D.Guild): Promise<[number, string | null]> {
-  let row = await ctx.db.get(
-        'SELECT count, lastbumped FROM count WHERE GUILD = ?',
-    [ 'guild:' + guild.id ]);
+function currentCount(ctx: Context, guild: D.Guild): [number, string | null] {
+  let row = ctx.db.prepare('SELECT count, lastbumped FROM count WHERE GUILD = ?')
+    .get([ 'guild:' + guild.id ]);
 
   if (row === undefined) {
     return [ 0, null ];
@@ -44,66 +42,57 @@ async function currentCount(ctx: Context, guild: D.Guild): Promise<[number, stri
 
 // Returns the result of the guess, and the previous count
 // guess is a string, since it can be a ratio (1/2)
-async function incr(ctx: Context, user: D.User, guild: D.Guild, guess: string): Promise<[CountResult, number]> {
-  return await ctx.dbLock.runExclusive(async () => {
-    ctx.db.run("BEGIN");
-
+function incr(ctx: Context, user: D.User, guild: D.Guild, guess: string): [CountResult, number] {
+  return ctx.db.transaction((): [CountResult, number] => {
     let guildId = 'guild:' + guild.id;
     let userId = 'user:' + user.id;
 
-    let error: boolean = false;
-    try {
-      debug(`Incr for ${user} in ${guild}, guess ${guess}`);
+    debug(`Incr for ${user} in ${guild}, guess ${guess}`);
 
-      let [ count, lastbumped ] = await currentCount(ctx, guild);
+    let [ count, lastbumped ] = currentCount(ctx, guild);
 
-      debug(`Count was ${count}, lastbumped was ${lastbumped}`);
+    debug(`Count was ${count}, lastbumped was ${lastbumped}`);
 
-      if (userId === lastbumped) {
-        debug("User ignored: same as lastbumped");
-        return [ 'ignore', count ];
-      }
-      else if ((count + 1).toString() === guess) {
-        await ctx.db.run(`INSERT INTO count(guild, count, lastbumped) VALUES (:guild, :count, :user)
-                            ON CONFLICT DO UPDATE SET count = :count, lastbumped = :user`,
-                         {
-                              ':count': count + 1,
-                              ':user': userId,
-                              ':guild': guildId,
-                         });
-        await ctx.db.run(`INSERT INTO stats(guild, user, bumps, loss) VALUES (?, ?, 1, 0)
-                            ON CONFLICT DO UPDATE SET bumps = bumps + 1`,
-                         [ guildId, userId, ]);
-        debug("Bumped");
-        return [ 'bump', count ];
-      }
-      else if (count === 0) {
-        debug("User ignored: bad guess, count was 0");
-        return [ 'ignore', count ];
-      }
-      else {
-        await ctx.db.run(`INSERT INTO count(guild, count, lastbumped) VALUES (:guild, 0, null)
-                            ON CONFLICT DO UPDATE SET count = 0, lastbumped = NULL`,
-                         { ':guild': guildId });
-        await ctx.db.run(`INSERT INTO stats(guild, user, bumps, loss) VALUES (?, ?, 0, 1)
-                            ON CONFLICT DO UPDATE SET loss = loss + 1`,
-                   [ guildId, userId ]);
-        debug("Soiled it");
-        return [ 'loss', count ];
-      }
+    if (userId === lastbumped && !process.argv.includes('--allow-repeats')) {
+      debug("User ignored: same as lastbumped");
+      return [ 'ignore', count ];
     }
-    catch (e) {
-      error = true;
-      throw e;
+    else if ((count + 1).toString() === guess) {
+      ctx.db.prepare(
+        "INSERT INTO count(guild, count, lastbumped) VALUES (:guild, :count, :user) " +
+          "ON CONFLICT DO UPDATE SET count = :count, lastbumped = :user")
+        .run({
+          count: count + 1,
+          user: userId,
+          guild: guildId,
+        });
+
+      ctx.db.prepare(
+        "INSERT INTO stats(guild, user, bumps, loss) VALUES (?, ?, 1, 0) " +
+          "ON CONFLICT DO UPDATE SET bumps = bumps + 1")
+        .run([ guildId, userId, ]);
+
+      debug("Bumped");
+      return [ 'bump', count ];
     }
-    finally {
-      if (error) {
-        ctx.db.run("ROLLBACK");
-      } else {
-        ctx.db.run("COMMIT");
-      }
+    else if (count === 0) {
+      debug("User ignored: bad guess, count was 0");
+      return [ 'ignore', count ];
     }
-  });
+    else {
+      ctx.db.prepare(
+        "INSERT INTO count(guild, count, lastbumped) VALUES (:guild, 0, null) " +
+          "ON CONFLICT DO UPDATE SET count = 0, lastbumped = NULL")
+        .run({ guild: guildId });
+
+      ctx.db.prepare(
+        "INSERT INTO stats(guild, user, bumps, loss) VALUES (?, ?, 0, 1)" +
+          "ON CONFLICT DO UPDATE SET loss = loss + 1")
+        .run([ guildId, userId ]);
+      debug("Soiled it");
+      return [ 'loss', count ];
+    }
+  })();
 }
 
 async function loserboard(ctx: Context, user: D.User, guild: D.Guild, prevCount: number): Promise<string> {
@@ -129,15 +118,15 @@ async function leaderboard(ctx: Context, guild: D.Guild): Promise<string> {
 
   let n = 0;
   let contributors = (await Promise.all(
-    (await ctx.db.all(`SELECT user, bumps FROM stats WHERE guild = ? ORDER BY bumps DESC LIMIT 10`,
-                      [ 'guild:' + guild.id ]))
+    ctx.db.prepare(`SELECT user, bumps FROM stats WHERE guild = ? ORDER BY bumps DESC LIMIT 10`)
+      .all([ 'guild:' + guild.id ])
       .map(async (row) => `${++n}: ${await nickname1(row.user)}, with ${row.bumps} bumps`)))
                        .join('\n');
 
   n = 0;
   let losers = (await Promise.all(
-    (await ctx.db.all(`SELECT user, loss FROM stats WHERE guild = ? ORDER BY loss DESC LIMIT 10`,
-                      [ 'guild:' + guild.id ]))
+    ctx.db.prepare(`SELECT user, loss FROM stats WHERE guild = ? ORDER BY loss DESC LIMIT 10`)
+      .all([ 'guild:' + guild.id ])
       .map(async (row) => `${++n}: ${await nickname1(row.user)}, with ${row.loss} losses`)))
                  .join('\n');
 
@@ -192,18 +181,14 @@ async function evalMessage(ctx: Context, msg: D.Message): Promise<void> {
     D.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
   ]});
 
-  let db = await sqlite.open({
-    filename: "test.db",
-    driver: sqlite3.Database,
-  });
+  let db = Database("test.db");
 
   var ctx: Context = {
     db: db,
-    dbLock: new Mutex(),
     discord: client,
   };
 
-  await setupDb(ctx);
+  setupDb(ctx);
 
   await client.login(process.env.BOT_TOKEN);
   debug("Logged in!");
