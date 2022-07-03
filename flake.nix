28a4692107b27cb3cb365b13e3eca94e83efe6ce
Rob Glossop <robgssp@gmail.com>
2022-07-03 19:14:22 +0000

    Add a nix build script

--- flake.nix
+++ flake.nix
@@ -6,16 +6,69 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, utils, ... }:
+  outputs = { self, nixpkgs, utils, ... }:
     utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+          nodeEnv = self.packages.${system}.nodeEnv;
+          nodeDeps = (import nodeEnv { inherit pkgs; }).nodeDependencies;
       in {
-        # packages.x86_64-linux.squawkbot = { };
+        packages = {
+          # save on rebuilds with __contentAddressed
+          srcsStripped = pkgs.runCommand "sources-stripped" {
+            __contentAddressed = true;
+            src = ./.;
+          } ''
+              mkdir $out
+              cp -a $src/package.json $src/package-lock.json $out
+          '';
 
-        # defaultPackage.x86_64-linux = self.packages.x86_64-linux.squawkbot;
+          nodeEnv = pkgs.stdenv.mkDerivation {
+            name = "node-env";
+            src = self.packages.${system}.srcsStripped;
+            buildInputs = with pkgs; [ nodejs sqlite node2nix ];
+
+            buildPhase = ''
+              node2nix -16 -l package-lock.json
+            '';
+            installPhase = ''
+              mkdir $out
+              cp package{,-lock}.json default.nix node-packages.nix node-env.nix $out
+            '';
+          };
+
+          squawkbot = pkgs.stdenv.mkDerivation {
+            name = "squawkbot";
+            src = ./.;
+            buildInputs = with pkgs; [ nodejs sqlite ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
+            buildPhase = ''
+              ln -s ${nodeDeps}/lib/node_modules node_modules
+
+              npm run build
+            '';
+            installPhase = ''
+              mkdir -p $out/lib $out/bin
+              cp -a dist node_modules $out/lib
+
+              cat > $out/bin/squawkbot <<EOF
+                #!/bin/sh
+                node $out/lib/dist/index.js "$@"
+              EOF
+              chmod a+x $out/bin/squawkbot
+
+              wrapProgram $out/bin/squawkbot \
+                --set PATH ${pkgs.lib.makeBinPath (with pkgs; [
+                  nodejs sqlite
+                ])}
+            '';
+          };
+
+          default = self.packages.${system}.squawkbot;
+        };
 
         devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.nodejs pkgs.sqlite-interactive ];
+          buildInputs = with pkgs; [ nodejs sqlite-interactive node2nix ];
         };
     });
 }
